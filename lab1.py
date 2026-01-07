d55343b3919f35e263c7c05e3075fbb362f24767
lowojoandro <lowojoandro@gmail.com>
2026-01-07 12:50:08 -0800

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,50 @@
+from typing import * 
+from dataclasses import dataclass 
+import unittest 
+import sys 
+sys.setrecursionlimit(10**6) 
+ 
+#* Team-Member Intro Lines 
+ 
+#* Data Definitions 
+ 
+#* 1) 
+ 
+#* 2) 
+ 
+#* 3) 
+ 
+#* 4) 
+ 
+#* Design Recipe 
+ 
+#* 1) 
+ 
+#* 2) 
+ 
+#* 3) 
+ 
+#* 4) 
+ 
+class TestClass(unittest.TestCase): 
+    def test_example(self): 
+        self.assertEqual( 1, 1 ) 
+ 
+    #* 1) 
+ 
+    #* 2) 
+ 
+    #* 3) 
+ 
+    #* 4) 
+ 
+# If this is True, means this .py file is the .py being executed  
+# (rather than being imported by the .py that is being executed). 
+if (__name__ == '__main__'):   
+    print( "Running all defined tests:" ) 
+ 
+    # What this does: find every class X that inherits from  
+    # unittest.TestCase (there are two in this file) and runs 
+    # every test defined inside X--every method whose name 
+    # begins with "test". 
+    unittest.main()
